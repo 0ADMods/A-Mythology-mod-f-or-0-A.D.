099d1b8eb89ec1df44b898ff279ec5988abfdd8f
tomasmatys <42038877+tomasmatys@users.noreply.github.com>
2019-01-21 15:55:02 +0100

    Create mainmenu_mythology.js

---
+++ gui/pregame/mainmenu_mythology.js
@@ -0,0 +1,16 @@
+const preMythologyInit = init;
+init = function(initData, hotloadData)
+{
+	preMythologyInit(initData, hotloadData);
+
+	// Remember to keep session/session_millenniumad.js in sync with this:
+	Engine.GetGUIObjectByName("mainMenu").children[4].children[0].caption = sprintf("%(name)s\n(%(version)s)\n\n%(warning)s", {
+		// Translation: Game/Mod name as displayed on lower part of the main menu seen on game start
+		"name": setStringTags(sprintf(translate("%(title)s:\n%(subtitle)s"), {
+			"title": translate("Mythology Mod"),
+			"subtitle": translate("Demo")
+		}), { "font": "sans-bold-16" }),
+		"version": translate("0 A.D. Alpha XXIII"), 
+		"warning": translate("WARNING: This is an early development version of the game. Many features have not been added yet.")
+	});
+}
