5f95e06411b9cc8a2c2e94d3838f90dade9f7d04
Stanislas Dolcini <stanislas.dolcini@gmail.com>
2022-07-07 19:40:33 +0200

    Fix error when starting match because of no AI names

--- gui/pregame/ProjectInformation_mythology.js
+++ gui/pregame/ProjectInformation_mythology.js
@@ -1,16 +1,3 @@
-const preMythologyInit = init;
-init = function(initData, hotloadData)
-{
-	preMythologyInit(initData, hotloadData);
-
-	// Remember to keep session/session_mythology.js in sync with this:
-	Engine.GetGUIObjectByName("mainMenu").children[4].children[0].caption = sprintf("%(name)s\n(%(version)s)\n\n%(warning)s", {
-		// Translation: Game/Mod name as displayed on lower part of the main menu seen on game start
-		"name": setStringTags(sprintf(translate("%(title)s:\n%(subtitle)s"), {
-			"title": translate("Mythology Mod"),
-			"subtitle": translate("Demo")
-		}), { "font": "sans-bold-16" }),
-		"version": translate("0 A.D. Alpha XXIV"), 
-		"warning": translate("WARNING: This is an early development version of the game. Many features have not been added yet.")
-	});
-}
+g_ProjectInformation.productDescription.caption =
+	setStringTags(translate("Mythology Mod v1.0.0"), { "font": "sans-bold-16" }) + "\n\n" +
+	translate("Notice: This mod is under development and many features have not been added yet.");
